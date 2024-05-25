d6475f4ace20f1c6a33bd9cc4c700e0eaa5df0a1
Boxuan Yang <u6778195@anu.edu.au>
2024-05-25 14:05:24 +1000

    sd

--- openmp/parAdvect.c
+++ openmp/parAdvect.c
@@ -16,17 +16,50 @@ void init_parallel_parameter_values(int M_, int N_, int P_, int Q_, int verb) {
   verbosity = verb;
 } //init_parallel_parameter_values()
 
-
+/*
+This function should be parallelized with the purpose of improving performance.
+*/
 void omp_update_boundary_1D_decomposition(double *u, int ldu) {
+  /* Original code
   int i,j;
   for (j = 1; j < N+1; j++) { //top and bottom halo
     u[j] = u[M * ldu + j];
     u[(M + 1) * ldu + j] = u[ldu + j];
   }
+
   for (i = 0; i < M+2; i++) { //left and right sides of halo 
     u[i * ldu] = u[i * ldu + N];
     u[i * ldu + N + 1] = u[i * ldu + 1];
   }
+  */
+  
+  
+
+  int upper = (M > N) ? M : N;
+  int i;
+
+  #pragma omp parallel for schedule(static)
+  for(i = 1; i <= upper; i++){
+    if(i <= N){
+      u[i] = u[M * ldu + i];
+      u[(M + 1) * ldu + i] = u[ldu + i];
+    }
+
+    if(i <= M){
+      u[i * ldu] = u[i * ldu + N];
+      u[i * ldu + N + 1] = u[i * ldu + 1];
+    }
+  }
+
+  u[0] = u[N];
+  u[(M+1) * ldu] = u[(M+1) * ldu + N];
+  u[N+1] = u[1];
+  u[(M+1) * ldu + N + 1] = u[(M+1) * ldu + 1];
+
+  return;
+
+
+
 } 
 
 void omp_update_advection_field_1D_decomposition(double *u, int ldu, double *v, int ldv) {
@@ -35,8 +68,24 @@ void omp_update_advection_field_1D_decomposition(double *u, int ldu, double *v, 
   double cim1, ci0, cip1, cjm1, cj0, cjp1;
   calculate_and_update_coefficients(Ux, &cim1, &ci0, &cip1); 
   calculate_and_update_coefficients(Uy, &cjm1, &cj0, &cjp1);
-  for (i=0; i < M; i++)
-    for (j=0; j < N; j++)
+
+  /*
+  We need to iterate all the combinations of:
+  1. Parallize out/inner loop
+  2. Interchange loop order
+  3. Schedule iterations with block/cyclic fashion
+
+  The openmp directive for combination 3 is:
+  #pragma omp parallel for schedule(static)
+  #pragma omp parallel for schedule(dynamic)
+
+  M,N are global variables
+  */
+  
+
+   // Unparallized version
+  for (i=0; i < M; i++){
+    for (j=0; j < N; j++){
       v[i * ldv + j] =
           cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
                   cjp1 * u[(i - 1) * ldu + j + 1]) +
@@ -44,15 +93,149 @@ void omp_update_advection_field_1D_decomposition(double *u, int ldu, double *v, 
                  cjp1 * u[i * ldu + j + 1]) +
           cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
                   cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  
+
+  /*
+  1. outer loop, i-j loop, dynamic
+  #pragma omp parallel for private(j) schedule(dynamic)
+  for (i=0; i < M; i++){
+    for (j=0; j < N; j++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
+  
+  /*
+  2. outer loop, i-j loop, static
+  #pragma omp parallel for private(j) schedule(static)
+  for (i=0; i < M; i++){
+    for (j=0; j < N; j++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
+
+  /*
+  3. outer loop, j-i loop, dynamic
+  #pragma omp parallel for private(i) schedule(dynamic)
+  for (j=0; j < N; j++){
+    for (i=0; i < M; i++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
+
+  /*
+  4. outer loop, j-i loop, static
+  #pragma omp parallel for private(i) schedule(static)
+  for (j=0; j < N; j++){
+    for (i=0; i < M; i++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
+
+  /*
+  5. inner loop, i-j loop, dynamic
+  for (i=0; i < M; i++){
+    #pragma omp parallel for schedule(dynamic)
+    for (j=0; j < N; j++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
+
+  /*
+  6. inner loop, i-j loop, static
+  for (i=0; i < M; i++){
+    #pragma omp parallel for schedule(static)
+    for (j=0; j < N; j++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
+
+  /*
+  7. inner loop, j-i loop, dynamic
+  for (j=0; j < N; j++){
+    #pragma omp parallel for schedule(dynamic)
+    for (i=0; i < M; i++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
+
+  /*
+  8. inner loop, j-i loop, static
+  for (j=0; j < N; j++){
+    #pragma omp parallel for schedule(static)
+    for (i=0; i < M; i++){
+      v[i * ldv + j] =
+          cim1 * (cjm1 * u[(i - 1) * ldu + j - 1] + cj0 * u[(i - 1) * ldu + j] +
+                  cjp1 * u[(i - 1) * ldu + j + 1]) +
+          ci0 * (cjm1 * u[i * ldu + j - 1] + cj0 * u[i * ldu + j] +
+                 cjp1 * u[i * ldu + j + 1]) +
+          cip1 * (cjm1 * u[(i + 1) * ldu + j - 1] + cj0 * u[(i + 1) * ldu + j] +
+                  cjp1 * u[(i + 1) * ldu + j + 1]);
+    }
+  }
+  */
 
 } //omp_update_advection_field_1D_decomposition()  
 
 
 void omp_copy_field_1D_decomposition(double *in, int ldin, double *out, int ldout) {
   int i, j;
-  for (i=0; i < M; i++)
-    for (j=0; j < N; j++)
+  #pragma omp for private(j) schedule(static)
+  for (i=0; i < M; i++){
+    for (j=0; j < N; j++){
       out[i * ldout + j] = in[i * ldin + j];
+    }
+  }
 } //omp_copy_field_1D_decomposition()
 
 
